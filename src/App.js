2d3d5a75635033bf892e937dab45b9592e492e58
Jairo Cruz <jairolucianocruz@gmail.com>
2022-11-03 20:28:26 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react"
 import Header from "./Header"
 import Main from "./Main"
 import Footer from "./Footer"
-
+import { BrowserRouter } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 
 function App() {
 
     return (
-        <div>
+        <BrowserRouter>
             <Header/>
             <Main />
             <Footer />
             <ToastContainer/>
-        </div>
+        </BrowserRouter>
     )
 }
 
